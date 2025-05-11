bede5aafa387c45667314c90a3185e38bf4bf984
Soham7997 <161009997+Soham7997@users.noreply.github.com>
2025-05-11 19:58:39 +0530

    Update Logo.tsx

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo: React.FC = () => {
   return (
     <img 
-      src="image-removebg-preview%20(1).png"
+      src="src/image-removebg-preview (1).png"
       alt="URJA Japanese Learning & Consulting" 
       className="h-18 w-auto"
       style={{
